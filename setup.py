74bf684de3c255099334f25eaa3822ab96a8918f
Peter Hagen <pha@uber.com>
2019-11-06 11:15:46 +0100

    Bump version to 1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'pylint-starlark-plugin',         # How you named your package folder (MyLib)
   packages = ['pylint_starlark_plugin'],   # Chose the same as "name"
-  version = '0.10',      # Start with a small number and increase it with every change you make
+  version = '1.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Run pylint on Starklark files.',   # Give a short description about your library
   long_description='''
@@ -15,7 +15,7 @@ To use it, run `pip3 install pylint-starlark-plugin`, then you can call pytlint 
   author = 'Peter Hagen',                   # Type in your name
   author_email = 'peter@phgn.io',      # Type in your E-Mail
   url = 'https://github.com/phgn0/pylint-starlark-plugin',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/phgn0/pylint-starlark-plugin/archive/v0.2.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/phgn0/pylint-starlark-plugin/archive/1.0.tar.gz',    # I explain this later on
   keywords = ['pylint', 'starlark', 'plugin'],   # Keywords that define your package best
   install_requires=[
     'jedi',
