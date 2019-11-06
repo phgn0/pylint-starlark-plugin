56420f46da4c2fddf3f00866f8f0e066fc43836f
Peter Hagen <pha@uber.com>
2019-11-06 21:08:22 +0100

    Fix another readme typo

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Pylint Starlark Plugin
 
-This plugin allows you to run pylint on Starklark files, by doing an AST transform of load() calls into python import statements.
+This plugin allows you to run pylint on Starlark files, by doing an AST transform of load() calls into python import statements.
 
 To use it, run `pip3 install pylint-starlark-plugin`, then you can call pytlint via `pylint --load-plugins pylint_starlark_plugin file.py`.
--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 setup(
   name = 'pylint-starlark-plugin',         # How you named your package folder (MyLib)
   packages = ['pylint_starlark_plugin'],   # Chose the same as "name"
-  version = '1.0.1',      # Start with a small number and increase it with every change you make
+  version = '1.0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Run pylint on Starklark files.',   # Give a short description about your library
+  description = 'Run pylint on Starlark files.',   # Give a short description about your library
   long_description='''
-This plugin allows you to run pylint on Starklark files, by doing an AST transform of load() calls into python import statements.
+This plugin allows you to run pylint on Starlark files, by doing an AST transform of load() calls into python import statements.
 
 To use it, run `pip3 install pylint-starlark-plugin`, then you can call pytlint via `pylint --load-plugins pylint_starlark_plugin file.py`.
   ''',
@@ -15,14 +15,14 @@ To use it, run `pip3 install pylint-starlark-plugin`, then you can call pytlint 
   author = 'Peter Hagen',                   # Type in your name
   author_email = 'peter@phgn.io',      # Type in your E-Mail
   url = 'https://github.com/phgn0/pylint-starlark-plugin',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/phgn0/pylint-starlark-plugin/archive/1.0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/phgn0/pylint-starlark-plugin/archive/1.0.2.tar.gz',    # I explain this later on
   keywords = ['pylint', 'starlark', 'plugin'],   # Keywords that define your package best
   install_requires=[
     'jedi',
     'astroid',
   ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
